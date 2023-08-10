467236f29779e2af8dbe42ecb37795363325d6d4
Alex <mendozaalex24@gmail.com>
2023-08-09 23:08:24 -0300

    footer anchor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {FiInstagram,FiTwitter,FiFacebook} from 'react-icons/fi'
+import { Link as Anchor } from 'react-router-dom'
 
 export default function Footer() {
   return (
     <footer className='flex flex-row justify-between m-4'>
       <p> &copy; 2023 -Todos los derechos reservados </p>
       <div className='flex flex-row gap-4'>
-        <a href='#'><FiInstagram /></a>
-        <a href='#'><FiTwitter /></a>
-        <a href='#'><FiFacebook /></a>
+        <Anchor to='#'><FiInstagram /></Anchor>
+        <Anchor to='#'><FiTwitter /></Anchor>
+        <Anchor to='#'><FiFacebook /></Anchor>
       </div>
     </footer>
   )
