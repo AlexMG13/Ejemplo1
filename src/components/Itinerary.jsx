175dd5f4239c843033c01a70fe68ae48cb0c57b7
Alex <mendozaalex24@gmail.com>
2023-09-01 15:47:18 -0300

    redux for itinerary

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Itinerary({ itinerary }) {
   return (
-    <div className="card w-[80%] bg-primary text-primary-content">
+    <div className="card w-[80%] bg-primary text-cyan-800">
       <div className="card-body">
         <h2 className="card-title">{itinerary.name}</h2>
+        <h2 className="card-title">{itinerary.price}</h2>
       </div>
     </div>
   );
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,28 +3,33 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCity } from "../services/citiesQueries.js";
 import Itinerary from "../components/Itinerary.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import citiesActions from "../store/actions/Cities";
 
 export default function Details() {
-  const [city, setCity] = useState({});
+  let cityInStore = useSelector((store) => store.citiesReducer.city);
+  const dispatch = useDispatch();
   const { id } = useParams();
   useEffect(() => {
     getCity(id)
-      .then((response) => setCity(response))
+      .then((data) => dispatch(citiesActions.add_city(data)))
       .catch((err) => console.log(err));
   }, []);
   return (
     <div className="flex justify-center flex-col mx-10">
       <div className="card lg:card-side bg-base-100">
         <figure>
-          <img src={city.photo} alt={city.name} />
+          <img src={cityInStore.photo} alt={cityInStore.name} />
         </figure>
         <div className="card-body w-[70%] self-center">
-          <h2 className="card-title">{city.name}</h2>
-          <p>{city.description}</p>
+          <h2 className="card-title">{cityInStore.name}</h2>
+          <p>{cityInStore.description}</p>
         </div>
       </div>
       <div className="my-10 flex justify-center">
-        {<Itinerary itinerary={city.itineraries} />}
+        {cityInStore.itineraries?.map((itinerary, i) => (
+          <Itinerary itinerary={itinerary} key={i} />
+        ))}
       </div>
     </div>
   );
