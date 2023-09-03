388847e30d12b210908df4aecec1f3f15a2c136c
Alex <mendozaalex24@gmail.com>
2023-09-03 12:42:23 -0300

    adding image to itinerary

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -3,16 +3,23 @@ import { CiMoneyBill } from "react-icons/ci";
 
 export default function Itinerary({ itinerary }) {
   return (
-    <div className="h-24 bg-blue-400 rounded-lg m-7">
-      <h2 className="text-center font-bold text-xl">{itinerary.name}</h2>
-      <div className="flex justify-around my-4 ">
-        <div className="flex flex-row">
-          <div className="self-center">
-            <CiMoneyBill />
+    <div className="bg-blue-400 rounded-lg m-7">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-center font-bold text-xl">{itinerary.name}</h2>
+        <figure className="w-24 h-24 rounded-sm">
+          <img src={itinerary.photo} alt={itinerary.name} />
+        </figure>
+      </div>
+      <div>
+        <div className="flex flex-row justify-around my-4 items-center font-bold">
+          <div className="flex flex-row">
+            <div className="self-center">
+              <CiMoneyBill />
+            </div>
+            <p className=""> {itinerary.price}</p>
           </div>
-          <p className=""> {itinerary.price}</p>
+          <p>Duration: {itinerary.duration} hs</p>
         </div>
-        <p>Duration: {itinerary.duration} hs</p>
       </div>
     </div>
   );
